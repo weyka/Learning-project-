2c4dfa3ff86052c0d86690de37d59d68e6bca4e8
weyka <ssd.qqq@gmail.com>
2021-02-04 14:56:44 +0300

    Нарисовал иконки инвентаря. Добавил, пока, 3 пустых скрипта для этих же кнопок.

---
+++ Assets/Scripts/Inventory/Invetory.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invetory : MonoBehaviour
+{
+    public bool[] isFull; // будет проверять, заполнен ли наш слот в инвентаре
+    public GameObject[] slots; // наши слоты
+}
